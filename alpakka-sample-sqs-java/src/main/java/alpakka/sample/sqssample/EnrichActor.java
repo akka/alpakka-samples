a2eb8bf1713d6982ea243ed71f07ce37070010b4
Martynas Mickevičius <self@2m.lt>
2018-10-10 14:20:09 -0500

    Add a separate logger for the actor class

--- alpakka-sample-sqs-java/src/main/java/alpakka/sample/sqssample/EnrichActor.java
+++ alpakka-sample-sqs-java/src/main/java/alpakka/sample/sqssample/EnrichActor.java
@@ -1,14 +1,19 @@
 package alpakka.sample.sqssample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import akka.actor.AbstractActor;
 
 class EnrichActor extends AbstractActor {
+
+    final static Logger log = LoggerFactory.getLogger(EnrichActor.class);
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(Integer.class, message -> {
-                    Main.log.debug("actor received '{}'", message);
+                    log.debug("actor received '{}'", message);
                     sender().tell(new ActorResponseMsg(message.toString()), self());
                 })
                 .build();
