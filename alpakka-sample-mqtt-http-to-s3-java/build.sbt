bf63572a8b9b7c4b2498ca3a684c8ff7342722d6
Enno Runne <458526+ennru@users.noreply.github.com>
2019-03-05 11:10:34 +0100

    Use MQTT streaming to subscribe

--- alpakka-sample-mqtt-http-to-s3-java/build.sbt
+++ alpakka-sample-mqtt-http-to-s3-java/build.sbt
@@ -5,6 +5,7 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-slf4j" % "2.5.21",
   "com.typesafe.akka" %% "akka-http" % "10.1.7",
   "com.lightbend.akka" %% "akka-stream-alpakka-mqtt" % "1.0-M3",
+  "com.lightbend.akka" %% "akka-stream-alpakka-mqtt-streaming" % "1.0-M3",
   "com.lightbend.akka" %% "akka-stream-alpakka-s3" % "1.0-M3",
   "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8" % "2.9.8",
   "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.8",
--- alpakka-sample-mqtt-http-to-s3-java/src/main/java/alpakka/sample/triggereddownload/Main.java
+++ alpakka-sample-mqtt-http-to-s3-java/src/main/java/alpakka/sample/triggereddownload/Main.java
@@ -1,28 +1,34 @@
 package alpakka.sample.triggereddownload;
 
 import akka.Done;
+import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.ContentTypes;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.Uri;
+import akka.japi.JavaPartialFunction;
 import akka.japi.Pair;
 import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
-import akka.stream.alpakka.mqtt.MqttConnectionSettings;
-import akka.stream.alpakka.mqtt.MqttQoS;
-import akka.stream.alpakka.mqtt.MqttSubscriptions;
-import akka.stream.alpakka.mqtt.javadsl.MqttSource;
+import akka.stream.OverflowStrategy;
+import akka.stream.alpakka.mqtt.streaming.*;
+import akka.stream.alpakka.mqtt.streaming.javadsl.ActorMqttClientSession;
+import akka.stream.alpakka.mqtt.streaming.javadsl.Mqtt;
+import akka.stream.alpakka.mqtt.streaming.javadsl.MqttClientSession;
 import akka.stream.alpakka.s3.javadsl.S3;
-import akka.stream.javadsl.Sink;
-import akka.stream.javadsl.Source;
+import akka.stream.javadsl.*;
+import akka.util.ByteString;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import scala.Option;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.CompletionStage;
 
 public class Main {
 
@@ -54,21 +60,46 @@ public class Main {
     }
 
     void run() throws Exception {
-        final MqttConnectionSettings mqttConnectionSettings =
-                MqttConnectionSettings
-                        .create(
-                                mqttBroker,
-                                "upload-control",
-                                new MemoryPersistence()
-                        );
+        MqttSessionSettings settings = MqttSessionSettings.create();
+        MqttClientSession mqttClientSession = ActorMqttClientSession.create(settings, materializer, system);
 
-        @SuppressWarnings("unchecked")
-        MqttSubscriptions mqttSubscriptions =
-                MqttSubscriptions.create(topic, MqttQoS.atLeastOnce());
+        Flow<ByteString, ByteString, CompletionStage<Tcp.OutgoingConnection>> tcpConnection =
+                Tcp.get(system).outgoingConnection("localhost", 1883);
 
-        MqttSource
-                .atMostOnce(mqttConnectionSettings, mqttSubscriptions, 8)
+        // Run MQTT via TCP
+        Flow<Command<Object>, DecodeErrorOrEvent<Object>, NotUsed> mqttFlow =
+                Mqtt.clientSessionFlow(mqttClientSession).join(tcpConnection);
+
+        LoggingAdapter logging = Logging.getLogger(system, this);
+
+        Pair<SourceQueueWithComplete<Command<Object>>, Source<DecodeErrorOrEvent<Object>, NotUsed>> run =
+                Source.<Command<Object>>queue(3, OverflowStrategy.fail())
+                        .log("Command sending", logging)
+                        .via(mqttFlow)
+                        .log("Received", logging)
+                        .toMat(BroadcastHub.of(DecodeErrorOrEvent.classOf()), Keep.both())
+                        .run(materializer);
+
+        SourceQueueWithComplete<Command<Object>> commands = run.first();
+        Source<DecodeErrorOrEvent<Object>, NotUsed> subscription = run.second();
+
+        CompletionStage<Done> receiver = subscription
+                .log("Subscription received", logging)
+                .collect( // we are only interested in `Publish` events
+                        new JavaPartialFunction<DecodeErrorOrEvent<Object>, Publish>() {
+                            @Override
+                            public Publish apply(DecodeErrorOrEvent<Object> x, boolean isCheck) {
+                                if (x.getEvent().isPresent() && x.getEvent().get().event() instanceof Publish) {
+                                    Publish publish = (Publish) x.getEvent().get().event();
+                                    // Acknowledge received Publish
+                                    Option<Integer> packetIdOption = publish.packetId().map(i -> i.underlying());
+                                    commands.offer(new Command<>(new PubAck(packetIdOption.get())));
+                                    return publish;
+                                } else throw noMatch();
+                            }
+                        })
                 .map(m -> m.payload().utf8String())
+                .log("URL received", logging)
                 .<DownloadCommand>map(downloadCommandReader::readValue)
                 .mapAsync(4, info -> {
                             String s3BucketKey = createS3BucketKey(info);
@@ -77,11 +108,25 @@ public class Main {
                                     .mapAsync(1, http::singleRequest)
                                     .flatMapConcat(httpResponse -> httpResponse.entity().getDataBytes())
                                     .runWith(S3.multipartUpload(s3Bucket, s3BucketKey, ContentTypes.TEXT_HTML_UTF8), materializer)
-                                    .runWith(Sink.ignore(), materializer);
+                                    .runWith(Sink.ignore(), materializer)
+                                    .thenApply(done -> info.url);
                         }
                 )
-                .runForeach(res -> System.out.println(res), materializer)
-                .exceptionally(e -> { e.printStackTrace(); return Done.done(); });
+                .runWith(Sink.foreach(res -> System.out.println(res)), materializer)
+                .exceptionally(e -> {
+                    e.printStackTrace();
+                    return Done.done();
+                });
+
+        // let MQTT connect and subscribe to the topic
+        commands.offer(new Command<>(new Connect("upload-control2", ConnectFlags.CleanSession())));
+        commands.offer(new Command<>(new Subscribe(topic)));
+
+        Thread.sleep(60000);
+        commands.offer(new Command<>(Disconnect$.MODULE$));
+        Thread.sleep(5000);
+        logging.debug("Shutting down");
+        system.terminate();
     }
 
 }
