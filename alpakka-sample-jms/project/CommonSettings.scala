460ab38908636e1fe54800f797822c9ddc6a7a8a
Enno Runne <458526+ennru@users.noreply.github.com>
2019-08-09 09:56:48 +0200

    Add JBoss repository

--- alpakka-sample-jms/project/CommonSettings.scala
+++ alpakka-sample-jms/project/CommonSettings.scala
@@ -16,7 +16,9 @@ object CommonSettings {
     parallelExecution in ThisBuild := false,
     parallelExecution in GlobalScope := false,
     fork in Test := true,
-    libraryDependencies ++= Dependencies.dependencies
+    libraryDependencies ++= Dependencies.dependencies,
+    // Having JBoss as a first resolver is a workaround for https://github.com/coursier/coursier/issues/200
+    externalResolvers := ("jboss" at "http://repository.jboss.org/nexus/content/groups/public") +: externalResolvers.value
   ) ++
     AdditionalSettings.initialCmdsConsole ++
     AdditionalSettings.initialCmdsTestConsole ++
