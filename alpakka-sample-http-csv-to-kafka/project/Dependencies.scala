3821b2be2e9bad7342216f5dfe456df45babc37f
Enno Runne <458526+ennru@users.noreply.github.com>
2019-11-08 12:00:37 +0100

    Current versions

--- alpakka-sample-http-csv-to-kafka/project/Dependencies.scala
+++ alpakka-sample-http-csv-to-kafka/project/Dependencies.scala
@@ -1,25 +1,26 @@
 import sbt._
 
 object Dependencies {
-  val scalaVer = "2.12.8"
+  val scalaVer = "2.12.10"
   // #dependencies
   val ScalaTestVersion = "3.0.6"
-  val AkkaVersion = "2.5.21"
-  val AkkaHttpVersion = "10.1.8"
-  val AlpakkaVersion = "1.0.0"
-  val AlpakkaKafkaVersion = "1.0.3"
+  val AkkaVersion = "2.6.0"
+  val AkkaHttpVersion = "10.1.10"
+  val AlpakkaVersion = "1.1.2"
+  val AlpakkaKafkaVersion = "1.1.0"
 
   val dependencies = List(
     "com.lightbend.akka" %% "akka-stream-alpakka-csv" % AlpakkaVersion,
     "com.typesafe.akka" %% "akka-stream-kafka" % AlpakkaKafkaVersion,
+    "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
     "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
     // Used from Scala
     "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
     // Used from Java
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8" % "2.9.9",
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.9",
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8" % "2.10.0",
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.10.0",
 
-    "org.testcontainers" % "kafka" % "1.11.1",
+    "org.testcontainers" % "kafka" % "1.12.3",
     
     "com.typesafe.akka" %% "akka-slf4j" % AkkaVersion,
     "ch.qos.logback" % "logback-classic" % "1.2.3"
