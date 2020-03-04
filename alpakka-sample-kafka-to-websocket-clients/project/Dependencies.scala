f554ba0133a8878441e7dc495b4835fd3cf5148c
Enno Runne <458526+ennru@users.noreply.github.com>
2020-03-04 19:37:20 +0100

    Remove akka-typed

--- alpakka-sample-kafka-to-websocket-clients/project/Dependencies.scala
+++ alpakka-sample-kafka-to-websocket-clients/project/Dependencies.scala
@@ -9,7 +9,6 @@ object Dependencies {
   // #deps
   val dependencies = List(
     // #deps
-    "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
     "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
     "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
     "com.typesafe.akka" %% "akka-stream-kafka" % AlpakkaKafkaVersion,
