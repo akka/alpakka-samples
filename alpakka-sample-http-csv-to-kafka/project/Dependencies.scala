68f45745058cc7483b40a0905ed5fcfd57efc7dd
Enno Runne <458526+ennru@users.noreply.github.com>
2020-03-24 13:16:31 +0100

    Latest versions in HTTP CSV to Kafka

--- alpakka-sample-http-csv-to-kafka/project/Dependencies.scala
+++ alpakka-sample-http-csv-to-kafka/project/Dependencies.scala
@@ -4,10 +4,10 @@ object Dependencies {
   val scalaVer = "2.13.1"
   // #dependencies
   val ScalaTestVersion = "3.1.0"
-  val AkkaVersion = "2.6.1"
+  val AkkaVersion = "2.6.4"
   val AkkaHttpVersion = "10.1.11"
-  val AlpakkaVersion = "1.1.2"
-  val AlpakkaKafkaVersion = "2.0.1"
+  val AlpakkaVersion = "2.0.0-RC1"
+  val AlpakkaKafkaVersion = "2.0.2"
 
   val dependencies = List(
     "com.lightbend.akka" %% "akka-stream-alpakka-csv" % AlpakkaVersion,
@@ -18,10 +18,10 @@ object Dependencies {
     // Used from Scala
     "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
     // Used from Java
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8" % "2.10.0",
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.10.0",
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8" % "2.10.3",
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.10.3",
 
-    "org.testcontainers" % "kafka" % "1.12.4",
+    "org.testcontainers" % "kafka" % "1.13.0",
     
     "com.typesafe.akka" %% "akka-slf4j" % AkkaVersion,
     "ch.qos.logback" % "logback-classic" % "1.2.3"
